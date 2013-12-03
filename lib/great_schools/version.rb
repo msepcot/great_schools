1f486f81693962b13048a4e83a163025b21fda7b
Michael Sepcot <msepcot@tunein.com>
2013-12-02 22:01:11 -0800

    specify minimum ruby version of 1.9.3

--- lib/great_schools/version.rb
+++ lib/great_schools/version.rb
@@ -2,7 +2,7 @@ module GreatSchools # :nodoc:
   class Version # :nodoc:
     MAJOR = 0 # version when you make incompatible API changes
     MINOR = 1 # version when you add functionality in a backwards-compatible manner
-    PATCH = 0 # version when you make backwards-compatible bug fixes
+    PATCH = 1 # version when you make backwards-compatible bug fixes
 
     class << self # Class methods
       # MAJOR.MINOR.PATCH per Semantic Versioning 2.0.0
